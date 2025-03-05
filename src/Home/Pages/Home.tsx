3a54db6ebc37fe7cbe955cf963626a12291be2bd
gmlwlsdl <heejin094@naver.com>
2025-03-05 14:09:29 +0900

    chore: 기초 작업     - 폴더 구조 잡기     - nativewind 설정

---
+++ src/Home/Pages/Home.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native'
+
+export default function Home() {
+  return (
+    <View className="w-full h-full display flex justify-center bg-white">
+      <Text className="text-rose-500">
+        Open up App.tsx to start working on your app!
+      </Text>
+      {/* <StatusBar style="auto" /> */}
+    </View>
+  )
+}
